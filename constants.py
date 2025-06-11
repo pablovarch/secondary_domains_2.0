7b5c6523864aa4a582ceb32ee687a5dc58f033b5
pablovarch <pablo.a.varas@contenthound.com>
2025-06-10 23:29:21 -0300

    update constants

--- constants.py
+++ constants.py
@@ -4,14 +4,9 @@ name_csv_country_oxy = 'oxylabs_country.csv'
 
 crawler_version = '2023.08_auto'
 
-log_file = {'log_name': 'domain disc log'}
+log_file = {'log_name': 'sec domain crawler log'}
 
-proxy_dict = {'proxy_residential': 'lum-customer-c_f416ecd9-zone-residential-country-{}:4ar4mlv8uu9i@zproxy.lum-superproxy.io:22225',
-              'proxy_mobile': 'brd-customer-hl_f416ecd9-zone-mobile-country-{}:52ggi1lw5ei1@brd.superproxy.io:22225'}
 
-proxy_host = 'lum-customer-c_f416ecd9-zone-residential-country'
-proxy_server = 'http://zproxy.lum-superproxy.io:22225'
-proxy_password = '4ar4mlv8uu9i'
 
 browser = "MicrosoftEdge"
 
